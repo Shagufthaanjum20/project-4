1fe2dd79bbc460e5dc3866f2cf89d71ba43a191d
shagufthaanjum20 <shagufthanjum13@gmail.com>
2023-07-25 22:00:45 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def zain(request):
+    return HttpResponse('<marquee><h1>zain.by za</h1></marquee>')
+
+def zain(request):
+    return HttpResponse('<marquee><h1> zain  my crush</h2></marquee>')
